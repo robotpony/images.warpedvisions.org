f3861289128498945efcedf6b21536c158bebde6
Bruce Alderson <mx@warpedvisions.org>
2025-07-27 12:12:33 -0700

    Adds images used by warpedvisions.org over the past ~30 years. It's a mess.

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+rsync -avz --delete .  w28@w42.warpedvisions.org:~/images.warpedvisions.org/
---
+++ magiclink.php
@@ -0,0 +1,2 @@
+<br />
+<b>Fatal error</b>:  Call to undefined function:  add_filter() in <b>/home/.snookie/madmaxx/warpedvisions.org/wp-content/plugins/magiclink.php</b> on line <b>148</b><br />
