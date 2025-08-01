7c83ca70fd3478985113489dfdab8dae78152282
Bruce Alderson <mx@warpedvisions.org>
2025-07-31 17:43:17 -0700

    Filters out gitignores as well.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-rsync -avz --exclude .DS_Store --exclude .git/ --exclude deploy.sh --delete .  w28@w42.warpedvisions.org:~/images.warpedvisions.org/
+rsync -avz --exclude .gitignore --exclude .DS_Store --exclude .git/ --exclude deploy.sh --delete .  w28@w42.warpedvisions.org:~/images.warpedvisions.org/
